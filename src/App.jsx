65c8aa31af8ecd6264bc7c0eb7505fd866c2c034
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-01-08 11:02:22 +0530

    update-two-sate

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
 import { useState } from "react"
-
 const App = () => {
-  const [firstname, setfirstname] = useState(" ")
-  const [lasname, setlasname] = useState(" ")
+const [firstname, setFirstname] = useState(' ')
+const [lastname, setlastname] = useState(' ')
 
+function firstnameupdate(e){
+  setFirstname(e.target.value)
+}
 
-  function firstnameupdate(e) {
-    setfirstname(e.target.value)
-  }
+function lastnameupdate(e){
+  setlastname(e.target.value)
+}
 
-  function lastnameupdate(e) {
-    setlasname(e.target.value)
-  }
-
-
-  let fullname = firstname + " " + lasname
+let fullname = firstname + " " + lastname
   return (
     <>
-      <h1> All About name</h1>
+      <h1>if you click name than update four full name</h1>
       <label>
         First Name: {' '}
         <input 
         value={firstname}
         onChange={firstnameupdate}
-         />
+        />
       </label>
       <label>
         Last Name: {' '}
         <input 
-        value={lasname}
+        value={lastname}
         onChange={lastnameupdate}
-         />
+        />
       </label>
-      <p>your fullname is : {fullname}</p>
+      <p>Full name : {fullname}</p>
     </>
   )
 }
